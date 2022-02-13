a5cac69d594707a4bb7c5abfbd4166cf3c8245f8
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-02-13 15:06:57 +0300

    Homework Task3. The multiplication table with correction

--- src/berezyak/vadim/Main2.java
+++ src/berezyak/vadim/Main2.java
@@ -2,10 +2,11 @@ package berezyak.vadim;
 
 public class Main2 {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <10; i++) {
             for (int j = 1; j < 10 ; j++) {
+                System.out.printf("%-1d*%1d=%2d", j, i, j*i);
+                System.out.print("  ");
 
-                System.out.print(j + "*" + i + "=" + j*i + "  ");
 
             }
             System.out.println();
