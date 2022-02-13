5a9ae1ddff329ac66bd161a11c7ad7d3cc357700
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-02-13 14:27:26 +0300

    Homework Task3. The multiplication table

---
+++ src/berezyak/vadim/Main2.java
@@ -0,0 +1,15 @@
+package berezyak.vadim;
+
+public class Main2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10 ; j++) {
+
+                System.out.print(j + "*" + i + "=" + j*i + "  ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
