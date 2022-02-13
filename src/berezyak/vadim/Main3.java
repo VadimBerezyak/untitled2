29ed77ea70d99bd3f32fdbb0e59d9d1e6e13199c
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-02-13 15:34:30 +0300

    Homework Task3. Arith and geom progressions with corrections

--- src/berezyak/vadim/Main3.java
+++ src/berezyak/vadim/Main3.java
@@ -5,6 +5,7 @@ public class Main3 {
         int a1 = 1; // arithmetic progression
         int d = 5;
         int n = 10;
+        System.out.println("Arithmetic progression:");
         for (int i = 1; i <= n ; i++) {
             System.out.println("a" + i + " = " + a1 );
             a1 = a1 + d;
@@ -13,6 +14,7 @@ public class Main3 {
         int b1 = 2; // geometric progression
         int q = 2;
         int m = 10;
+        System.out.println("Geometric progression:");
         for (int j = 1; j <= m ; j++) {
             System.out.println("b" + j + " = " + b1 );
             b1 = b1 * 2;
